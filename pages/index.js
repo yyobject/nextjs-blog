ac835bf9b608f8b7e9135fe6fd2b700c981c1b78
wangyaoyao <wangyaoyao@tezign.com>
2025-04-22 18:15:25 +0800

    删除三个...

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home({allPostsData}) {
   return (
     <Layout home>
       <Head></Head>
-      <section className={utilStyles.headingMd}>…</section>
+      <section className={utilStyles.headingMd}></section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
